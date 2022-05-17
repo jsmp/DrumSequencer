3307148f34634d6411fb8e55965d445449cfafc6
João Sampaio <joao.sampaio@me.com>
2022-05-17 14:03:02 +0100

    Initial Commit

---
+++ Drum Sequencer/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  Drum Sequencer
+//
+//  Created by João Sampaio on 17/05/2022.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
